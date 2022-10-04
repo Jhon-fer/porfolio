7455669228b1a16aa3b8f12c8362cd31445b131f
Jhon Rios <r.jhonf@gmail.com>
2022-10-04 18:59:45 -0500

    adding attribute download

--- src/components/Introduce.jsx
+++ src/components/Introduce.jsx
@@ -11,7 +11,7 @@ const Introduce = () => {
           <h1 className="introduce--name">Jhon Rios</h1>
           <h2 className="introduce--title">Front End Developer</h2>
           <p className="introduce--text">Hola, soy desarrollador de software enfocado en aplicaciones y sitios web. Ven y conóceme para que seamos equipo y juntos solucionar las necesidades del mundo tecnologíco.</p>
-          <a href="../assets/docs/CV.pdf" className="download" download="curriculum-vitae.txt">Download CV</a>
+          <a href="../assets/docs/CV.pdf" className="download" download="curriculum-vitae">Download CV</a>
         </div>
         <img src={slider} alt="image-developer"/>
       </div>
